4c5cbe44d87edaee1bb25a884ba17b8c09f48bf0
Carson.Ho <carson.ho@foxmail.com>
2018-01-21 22:45:31 +0800

    add

--- app/src/main/java/scut/carson_ho/androidinterview/MainActivity.java
+++ app/src/main/java/scut/carson_ho/androidinterview/MainActivity.java
@@ -2,7 +2,6 @@ package scut.carson_ho.androidinterview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,18 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 通过匿名内部类 实现多线程
-        new Thread() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(5000);
-                    Log.d(TAG, "执行了多线程");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }.start();
+        
     }
 }

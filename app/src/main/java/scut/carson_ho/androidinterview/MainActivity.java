ab80bea115906afe3afac53f58c49f17ca1b02e1
Carson.Ho <carson.ho@foxmail.com>
2018-02-07 14:15:52 +0800

    add

--- app/src/main/java/scut/carson_ho/androidinterview/MainActivity.java
+++ app/src/main/java/scut/carson_ho/androidinterview/MainActivity.java
@@ -2,19 +2,53 @@ package scut.carson_ho.androidinterview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TAG = "carson：";
-    public static final String TAG1 = "carson：";
-    
+
+    Button button1,button2;
+    ViewGroup myLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        myLayout = (LinearLayout)findViewById(R.id.my_layout);
+
+        myLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.d("TAG", "点击了ViewGroup");
+                return false;
+            }
+
+        });
+
+        // 2. 为按钮1设置监听事件
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TAG", "点击了button1");
+            }
+        });
+
+        // 3. 为按钮2设置监听事件
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TAG", "点击了button2");
+            }
+        });
 
     }
 }

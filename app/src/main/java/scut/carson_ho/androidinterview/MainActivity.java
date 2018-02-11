3a40b7b2967e431e213026e19374b4f53824656d
Carson.Ho <carson.ho@foxmail.com>
2018-02-11 08:57:42 +0800

    add

--- app/src/main/java/scut/carson_ho/androidinterview/MainActivity.java
+++ app/src/main/java/scut/carson_ho/androidinterview/MainActivity.java
@@ -1,9 +1,9 @@
 package scut.carson_ho.androidinterview;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -21,17 +21,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        String device_model = Build.MODEL; // 设备型号
+        String version_sdk = Build.VERSION.SDK; // 设备SDK版本
+        String version_release = Build.VERSION.RELEASE; // 设备的系统版本
+
+        System.out.println(device_model +version_sdk);
+        System.out.println(version_sdk);
+        System.out.println(version_release);
         button1 = (Button)findViewById(R.id.button1);
         button2 = (Button)findViewById(R.id.button2);
         myLayout = (LinearLayout)findViewById(R.id.my_layout);
 
-        myLayout.setOnTouchListener(new View.OnTouchListener() {
+        // 1.为ViewGroup布局设置监听事件
+        myLayout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
+            public void onClick(View v) {
                 Log.d("TAG", "点击了ViewGroup");
-                return false;
             }
-
         });
 
         // 2. 为按钮1设置监听事件

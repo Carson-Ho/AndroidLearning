023a3ad768ebf7f42f95d19d302b5a1eeb1c9a50
Carson.Ho <carson.ho@foxmail.com>
2018-02-14 16:49:30 +0800

    add

--- app/src/main/java/scut/carson_ho/androidinterview/HashMapTest.java
+++ app/src/main/java/scut/carson_ho/androidinterview/HashMapTest.java
@@ -5,7 +5,7 @@ import android.widget.Button;
 
 public class HashMapTest {
 
-    Button button1,button2;
+    Button button1,button2,button3,button4,button5,button6;
     ViewGroup myLayout;
    
 
